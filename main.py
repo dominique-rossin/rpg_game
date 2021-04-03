1e7f3cac6249edd22118a4aa7355bee68a6d747d
atomic-crafter <64207317+atomic-crafter@users.noreply.github.com>
2021-04-03 23:15:32 +0200

    map movement and scale(not finished)

--- main.py
+++ main.py
@@ -1,13 +1,89 @@
 import pygame
 from mymap import *
+from player import *
 
 pygame.init()
-screen = pygame.display.set_mode([500, 500])
+longueur,largeur = 0,0
+fps = 60
+screen = pygame.display.set_mode((longueur,largeur), pygame.FULLSCREEN)
+
+longueur, largeur = pygame.display.Info().current_w, pygame.display.Info().current_h
+
+longueur_virtuelle = 400
+largeur_virtuelle = 225
+
+coefficient_longueur = round(longueur/longueur_virtuelle)
+coefficient_largeur = round(largeur/largeur_virtuelle)
+
+clock = pygame.time.Clock()
+clock.tick(fps)
 
 m = Map()
 
+player = player()
+player.draw()
+
+
+
+
 running = True
+player_x, player_y = 0,0
+
+haut = False
+bas = False
+gauche = False
+droite = False
+
 while running:
+
+	for event in pygame.event.get():
+		if event.type == pygame.KEYDOWN:
+
+			if event.key == pygame.K_UP:
+				haut = True
+
+			if event.key == pygame.K_DOWN:
+				bas = True
+
+			if event.key == pygame.K_LEFT:
+				gauche = True
+
+			if event.key == pygame.K_RIGHT:
+				droite = True
+
+		if event.type == pygame.KEYUP:
+
+			if event.key == pygame.K_UP:
+				haut = False
+
+			if event.key == pygame.K_DOWN:
+				bas = False
+
+			if event.key == pygame.K_LEFT:
+				gauche = False
+
+			if event.key == pygame.K_RIGHT:
+				droite = False
+						
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			Fin = True
+
+	if haut:
+		player_y -= 0.5
+
+	if bas:
+		player_y += 0.5
+
+	if gauche:
+		player_x -= 0.5
+
+	if droite:
+		player_x += 0.5
+
+	m.scroll_x(player_x)
+	m.scroll_y(player_y)
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
--- player.py
+++ player.py
@@ -7,50 +7,7 @@ class player:
 		self.x = 0
 		self.y = 0
 
+
+
 	def draw(self):
-		for event in pygame.event.get():
-			if event.type == pygame.KEYDOWN:
-
-				if event.key == pygame.K_UP:
-					haut = True
-
-				if event.key == pygame.K_DOWN:
-					bas = True
-
-				if event.key == pygame.K_LEFT:
-					gauche = True
-
-				if event.key == pygame.K_RIGHT:
-					droite = True
-
-			if event.type == pygame.KEYUP:
-
-				if event.key == pygame.K_UP:
-					haut = False
-
-				if event.key == pygame.K_DOWN:
-					bas = False
-
-				if event.key == pygame.K_LEFT:
-					gauche = False
-
-				if event.key == pygame.K_RIGHT:
-					droite = False
-						
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				Fin = True
-
-
-			if haut:
-				self.y -= 0.5
-
-			if bas:
-				self.y += 0.5
-
-			if gauche:
-				self.x -= 0.5
-
-			if droite:
-				self.x += 0.5
-
+		pass
